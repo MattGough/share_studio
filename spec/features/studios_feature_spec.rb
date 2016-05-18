845142e00c59b1592d1aa6c6eb5264fd00081d28
Matt <mattpgough@gmail.com>
2016-05-18 11:39:34 +0100

    Added Studio model.

--- spec/features/studios_feature_spec.rb
+++ spec/features/studios_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'studios' do
       expect(page).to have_link 'Add a studio'
     end
   end
+
+  context 'studios have been added' do
+    before do
+      Studio.create(name: 'Premises')
+    end
+
+    scenario 'display studios' do
+      visit '/studios'
+      expect(page).to have_content('Premises')
+      expect(page).not_to have_content('No studios yet')
+    end
+  end
 end
